8c99616cd00ccdd02df6134bd3ab1de974783245
Diana Elena <diana.apolinar@hotmail.com>
2019-12-22 11:12:53 -0600

    Change referece

--- src/main/java/KafkaProducerConfig.java
+++ src/main/java/KafkaProducerConfig.java
@@ -1,31 +1,34 @@
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
-import java.util.Map;
+@Component
+class KafkaMessageProducer {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
-@Configuration
-public class KafkaProducerConfig {
+    @Value(value = "${message.topic.name:profesorp}")
+    private String topicName;
 
-    public ProducerFactory<String, String> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                bootstrapAddress);
-        configProps.put(
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
-        configProps.put(
-                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
+    public void sendMessage(String topic, final String message) {
+        if (topic == null || topic.trim().equals(""))
+            topic = topicName;
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, message);
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
 
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<String, String>(producerFactory());
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                System.err.println("Unable to send message=[" + message + "] due to : " + ex.getMessage());
+            }
+        });
     }
 }
